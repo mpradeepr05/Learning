572e5dcb0043afb8be762cfed5720294e3eb64db
Pradeep Rana <vmac@LTA1LGKHN-Mac.local>
2018-07-07 02:46:39 +0530

    ui fixed

---
+++ js3.js
@@ -0,0 +1,26 @@
+// var numb = 2;
+
+// for (let i = 0; i < 10; i++) {
+//     if(i==0){
+//         console.log(
+//             numb + " X " + i +" = 1"
+//         );
+//     }else{
+//         console.log(
+//             numb + " X " + i +" = "+ i*numb
+//         );
+//     }
+//     numb + " X " + i +" = "+ i*numb
+    
+// }
+
+// for (let i = 1; i < 20; i++) {
+//     if (i%2===0) {
+//         console.log(i+" this number is even")
+//     }
+//     else{
+//         console.log(i+" this number is odd")
+//     }
+// }
+
+
